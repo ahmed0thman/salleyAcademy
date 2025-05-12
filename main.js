cbc33c9dd879850692c4d042924b736244da25a2
ahmed0thman <ahmedneo117@gmail.com>
2025-05-12 12:55:59 +0300

    sallyAcademy

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./assets/libs/bootstrap/js/bootstrap.bundle.min.js";
+import "./shared/utilities/modal.js";
+import "./shared/layout/header/header.js";
+import "./pages/home/newsLetter.js";
+
+document.body.classList.add("no-transition");
+window.addEventListener("load", () => {
+  document.body.classList.remove("no-transition");
+});
---
+++ pages/instructors/instrucors.js
@@ -0,0 +1,57 @@
+import { animateOnView } from "/shared/utilities/helperFunctions.js";
+// import Swiper from "/assets/libs/swiper/swiper.min.js";
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+// start Instructors Section
+const swiperInstructors = new Swiper("#swiperInstructors", {
+  breakpoints: {
+    1400: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const selectedCInstructorsHeader = document.getElementById("instructorsHeader");
+
+if (selectedCInstructorsHeader) {
+  animateOnView(
+    selectedCInstructorsHeader,
+    { opacity: [0, 1], transform: ["translateY(50px)", "translateY(0px)"] },
+    { duration: 0.75, ease: "easeInOut", delay: 0.25 },
+    true,
+    "0px 0px -50px 0px"
+  );
+}
+
+// End Instructors Section
+
+//
